0c4aa0a767969f70050d06c4ca72ed8ad1b5eeb6
刘红 <liuhong1.happy@163.com>
2015-06-22 11:56:24 +0800

    Update test_discover.sh

--- test_discover.sh
+++ test_discover.sh
@@ -3,6 +3,7 @@ ETCD_HOST=$HOST_IP:4001
 
 docker run -it  --name discover --rm   \
 -e ETCD_HOST=$ETCD_HOST -e HOST_IP=$HOST_IP  \
+-v /var/docker_images/docker-discover:/code \
 -v /var/run:/var/run docker-discover /bin/bash
 
 python discover.py
