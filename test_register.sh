c5d09eb8ff414d546de1006ca747c565abdb3db5
刘红 <liuhong1.happy@163.com>
2015-06-24 22:33:41 +0800

    Update test_register.sh

--- test_register.sh
+++ test_register.sh
@@ -2,8 +2,8 @@ HOST_IP=$(hostname --all-ip-addresses | awk '{print $1}')
 ETCD_HOST=$HOST_IP:4001
 
 docker run -it  --name discover --rm   \
+--net host -p 127.0.0.1:1936:1936 \
 -e ETCD_HOST=$ETCD_HOST -e HOST_IP=$HOST_IP  \
--v /var/docker_images/docker-discover:/code \
 -v /var/run:/var/run docker-discover /bin/bash
 
 python register.py
