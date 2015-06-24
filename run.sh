bdd2071716db460d5f62ac78564fe74212938dc6
刘红 <liuhong1.happy@163.com>
2015-06-24 21:46:06 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ docker rm discover
 HOST_IP=$(hostname --all-ip-addresses | awk '{print $1}')
 ETCD_HOST=$HOST_IP:4001
 
-docker run -it --net host -d --name discover \
+docker run  -d -it --net host -p 127.0.0.1:1936:1936  \
 -e ETCD_HOST=$ETCD_HOST -e HOST_IP=$HOST_IP  \
 -v /var/docker_images/docker-discover:/code \
--v /var/run:/var/run docker-discover
+-v /var/run:/var/run --name discover docker-discover
