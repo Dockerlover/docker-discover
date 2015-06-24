561b03327d0efc934f01c39bcd969d5400e38ba2
刘红 <liuhong1.happy@163.com>
2015-06-24 22:48:51 +0800

    Update test_register.sh

--- test_register.sh
+++ test_register.sh
@@ -6,4 +6,4 @@ docker rm discover
 
 docker run -it --net host --name discover --rm -p 127.0.0.1:1936:1936 \
 -e ETCD_HOST=$ETCD_HOST -e HOST_IP=$HOST_IP  \
--v /var/run:/var/run docker-discover /bin/bash
+-v /var/run:/var/run -v /var/docker_images/docker-discover:/code docker-discover /bin/bash
