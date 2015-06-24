3c5732bf919f7e984f607c64cc5880017f0f80dc
刘红 <liuhong1.happy@163.com>
2015-06-24 23:31:02 +0800

    Update discover.py

--- discover.py
+++ discover.py
@@ -51,7 +51,7 @@ def get_services():
       continue
     service_keys = service_key[1:].split("/")
     service_values = service_value[1:].split("/")
-    if(len(service_keys)==4 and len(service_value)>0)
+    if(len(service_keys)==4 and len(service_value)>0):
       sub_domain = service_keys[2]+"."+service_keys[1]+"."+DOMAIN_NAME
       host_name = service_keys[3][0:12]
       for port in service_values:
